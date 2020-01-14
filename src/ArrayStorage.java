dd48104e9495bdae539e65be2165022e459eea82
ilya.kobelev <Ilya.Kobelev@acronis.com>
2020-01-14 22:56:21 +0300

    Решение HW1

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -3,28 +3,55 @@
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    int size = 0;
 
     void clear() {
+        for (int i = 0; i < size; i++)
+            storage[i] = null;
+        size = 0;
     }
 
     void save(Resume r) {
+        storage[size] = r;
+        size++;
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < size; i++) {
+            Resume resume = storage[i];
+            if (resume.uuid == uuid)
+                return resume;
+        }
         return null;
     }
 
     void delete(String uuid) {
+        int indexToDelete = -1;
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid == uuid) {
+                indexToDelete = i;
+                break;
+            }
+        }
+        if (indexToDelete >= 0) {
+            for (int i = indexToDelete; i < size; i++)
+                storage[i] = storage[i + 1];
+            size--;
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] resumes = new Resume[size];
+        for (int i = 0; i < size; i++) {
+            resumes[i] = storage[i];
+        }
+        return resumes;
     }
 
     int size() {
-        return 0;
+        return size;
     }
 }
